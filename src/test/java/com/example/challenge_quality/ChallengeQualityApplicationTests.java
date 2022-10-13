071f4b2726ed320c98e03a45bc7ec28c2c2cea67
GabFontesMeli <gabriel.fontes@mercadolivre.com>
2022-10-13 14:38:10 -0300

    initial setup

---
+++ src/test/java/com/example/challenge_quality/ChallengeQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challenge_quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeQualityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

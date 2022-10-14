77f4145bdd20e877c2d4d6d3dab1249ff681558d
mayumeli <mayu.satori@mercadolivre.com>
2022-10-14 15:06:50 -0300

    remove: remove dead code

--- src/main/java/com/example/challenge_quality/repository/DistrictRepository.java
+++ src/main/java/com/example/challenge_quality/repository/DistrictRepository.java
@@ -1,33 +1,14 @@
 package com.example.challenge_quality.repository;
 
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
 import com.example.challenge_quality.model.District;
 import com.example.challenge_quality.model.DistrictsData;
-import com.example.challenge_quality.util.NumberGenerator;
 
 @Repository
 public class DistrictRepository {
-    
-    // Map<Integer, District> districts = new HashMap<>();
-
-    // District lapa = new District((NumberGenerator.getInstance().getNext()), "Lapa", new BigDecimal("100.00"));
-    // District rocinha = new District((NumberGenerator.getInstance().getNext()), "Rocinha", new BigDecimal("90.00"));
-    // District copacabana = new District((NumberGenerator.getInstance().getNext()), "Copacabana", new BigDecimal("10000.00"));
-    // District liberdade = new District((NumberGenerator.getInstance().getNext()), "Liberdade", new BigDecimal("250.00"));
-
-
-    // public void createDistrict() {
-    //     districts.put((NumberGenerator.getInstance().getNext()), lapa);
-    //     districts.put((NumberGenerator.getInstance().getNext()), rocinha);
-    //     districts.put((NumberGenerator.getInstance().getNext()), copacabana);
-    //     districts.put((NumberGenerator.getInstance().getNext()), liberdade);
-    // }
 
     public District getDistrict(Integer id) {
         return DistrictsData.getInstance().getDistricts().get(id);

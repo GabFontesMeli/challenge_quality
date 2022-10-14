11d4757beb944e54e436aa6095b47583825177d1
franklinlopes <gabriel.franklin@mercadolivre.com>
2022-10-14 12:10:30 -0300

    wip(District): adding district data

--- src/main/java/com/example/challenge_quality/repository/DistrictRepository.java
+++ src/main/java/com/example/challenge_quality/repository/DistrictRepository.java
@@ -1,5 +1,6 @@
 package com.example.challenge_quality.repository;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,9 +14,21 @@ public class DistrictRepository {
     
     Map<Integer, District> districts = new HashMap<>();
 
+    District lapa = new District((NumberGenerator.getInstance().getNext()), "Lapa", new BigDecimal("100.00"));
+    District rocinha = new District((NumberGenerator.getInstance().getNext()), "Rocinha", new BigDecimal("90.00"));
+    District copacabana = new District((NumberGenerator.getInstance().getNext()), "Copacabana", new BigDecimal("10000.00"));
+    District liberdade = new District((NumberGenerator.getInstance().getNext()), "Liberdade", new BigDecimal("250.00"));
+
+
     public District createDistrict(District district) {
         districts.put(NumberGenerator.getInstance().getNext(), district);
         System.out.println(districts);
+
+        districts.put((NumberGenerator.getInstance().getNext()), lapa);
+        districts.put((NumberGenerator.getInstance().getNext()), rocinha);
+        districts.put((NumberGenerator.getInstance().getNext()), copacabana);
+        districts.put((NumberGenerator.getInstance().getNext()), liberdade);
+
         return district;
     }
 

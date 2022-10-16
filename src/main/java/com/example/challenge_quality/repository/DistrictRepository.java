ac7f2301fd4a57a0779c6f079afc2e7981fb25b3
GabFontesMeli <gabriel.fontes@mercadolivre.com>
2022-10-15 23:33:41 -0300

    refactor(District): changing districts storage to a json file

--- src/main/java/com/example/challenge_quality/repository/DistrictRepository.java
+++ src/main/java/com/example/challenge_quality/repository/DistrictRepository.java
@@ -1,22 +1,43 @@
 package com.example.challenge_quality.repository;
 
-import java.util.Map;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
+import com.example.challenge_quality.model.Property;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Repository;
 
 import com.example.challenge_quality.model.District;
-import com.example.challenge_quality.model.DistrictsData;
 
 @Repository
 public class DistrictRepository {
+    private final String path = "src/main/resources/Districts.json";
+    ObjectMapper mapper = new ObjectMapper();
+
+    public List<District> getAllDistricts() {
+        List<District> districts;
+
+        try {
+            districts = Arrays.asList(mapper.readValue(new File(path), District[].class));
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
+
+        return districts;
+    }
 
     public District getDistrict(Integer id) {
-        return DistrictsData.getInstance().getDistricts().get(id);
+        List<District> districts = getAllDistricts();
+        return districts.stream()
+                .filter(p -> p.getId() == id).findFirst()
+                .orElseThrow(() -> new RuntimeException("Property not found"));
     }
 
     public District getDistrictByName(String name) {
-        Map<Integer, District> districts = DistrictsData.getInstance().getDistricts();
-        return districts.entrySet()
-            .stream().filter(district -> district.getValue().getName().equals(name)).findFirst().get().getValue();
+        List<District> districts = getAllDistricts();
+        return districts.stream()
+                .filter(d -> d.getName().equals(name)).findFirst()
+                .orElseThrow(() -> new RuntimeException("District not found"));
     }
 }

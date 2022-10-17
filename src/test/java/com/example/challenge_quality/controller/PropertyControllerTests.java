56120942e92a2c65c7a815dbd0845fd4cb7e2277
htebet <henrique.tebet@mercadolivre.com>
2022-10-17 15:30:10 -0300

    test(Controller): getPropertyBiggestRoom method

--- src/test/java/com/example/challenge_quality/controller/PropertyControllerTests.java
+++ src/test/java/com/example/challenge_quality/controller/PropertyControllerTests.java
@@ -3,12 +3,15 @@ package com.example.challenge_quality.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import javax.print.attribute.standard.Media;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import com.example.challenge_quality.model.Room;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,8 @@ import com.example.challenge_quality.model.Property;
 import com.example.challenge_quality.service.PropertyService;
 import com.example.challenge_quality.setup.BasePropertyTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Optional;
 
 @WebMvcTest(PropertyController.class)
 public class PropertyControllerTests extends BasePropertyTest {
@@ -38,16 +43,32 @@ public class PropertyControllerTests extends BasePropertyTest {
         BDDMockito.given(service.createProperty(any(Property.class)))
         .willReturn(property);
         
-        String json = objectMapper.writeValueAsString(property);
+        String jsonExpected = objectMapper.writeValueAsString(property);
 
         this.mockMvc
             .perform(
                 post("/property")
-                    .content(json)
+                    .content(jsonExpected)
                     .contentType(MediaType.APPLICATION_JSON)
                 )
             .andExpect(status().isCreated())
-            .andExpect(content().json(json));
+            .andExpect(content().json(jsonExpected));
 
+    }
+
+    @Test
+    void getPropertyBiggestRoomShouldReturnPropertyBiggestRoom() throws Exception {
+        BDDMockito.given(service.getPropertyBiggestRoom(anyInt()))
+                .willReturn(Optional.of(property.getRooms().get(1)));
+
+        String jsonExpected = objectMapper.writeValueAsString(property.getRooms().get(1));
+
+        this.mockMvc
+                .perform(
+                        get("/property/biggest-room/{id}", 1)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().json(jsonExpected));
     }
 }

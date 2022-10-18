baac47e51fb2c80019fe45775919d7342ec7a437
mayumeli <mayu.satori@mercadolivre.com>
2022-10-17 21:12:28 -0300

    tests(PropertyIntegration): get property value tested

--- src/test/java/com/example/challenge_quality/integration/PropertyIntegrationTests.java
+++ src/test/java/com/example/challenge_quality/integration/PropertyIntegrationTests.java
@@ -6,11 +6,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.example.challenge_quality.setup.BasePropertyTest;
+import com.example.challenge_quality.model.District;
+import com.example.challenge_quality.model.Property;
+import com.example.challenge_quality.model.Room;
+import com.example.challenge_quality.repository.DistrictRepository;
+import com.example.challenge_quality.repository.PropertyRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.MediaType;
 
@@ -18,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class PropertyIntegrationTests extends BasePropertyTest {
+public class PropertyIntegrationTests {
     
     @Autowired
     private MockMvc mockMvc;
@@ -26,18 +35,67 @@ public class PropertyIntegrationTests extends BasePropertyTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private PropertyRepository propertyRepository;
+
+    @Autowired
+    private DistrictRepository districtRepository;
+
+    Property getNewProperty(String district) {
+        Property property;
+        String districtName = "Melicidade";
+        if (district != null) {
+            districtName = district;
+        }
+        List<Room> rooms = new ArrayList<>();
+        rooms.add(new Room(1, "Quarto", 2.0, 2.0));
+        rooms.add(new Room(2, "Cozinha", 3.0, 3.0));
+        rooms.add(new Room(3, "Banheiro", 1.0, 1.0));
+        property = new Property("Melicidade", districtName, rooms);
+        return property;
+    }
+
     @Test
     void createPropertyShouldReturnProperty() throws Exception {
 
-        String jsonExpected = objectMapper.writeValueAsString(property);
+        Property propertyPost = getNewProperty(null);
+        Property propertyExpected= getNewProperty(null);
+
+        int id = propertyRepository.getAll().size() + 1;
+
+        propertyExpected.setId(id);
+
+        String jsonExpected = objectMapper.writeValueAsString(propertyExpected);
+        String jsonPost = objectMapper.writeValueAsString(propertyPost);
 
         this.mockMvc
             .perform(
                 post("/property")
-                    .content(jsonExpected)
+                    .content(jsonPost)
                     .contentType(MediaType.APPLICATION_JSON)
                 )
             .andExpect(status().isCreated())
+            .andExpect(content().json(jsonExpected));
+    }
+
+    @Test
+    void getPropertyValueShouldReturnValue() throws Exception {
+
+        int randomNumber = (int) (Math.random() * 1000);
+        String randomDistrictName = "Melicidade" + randomNumber;
+        Property newProperty = getNewProperty(randomDistrictName);
+        districtRepository.createDistrict(new District(null, randomDistrictName, new BigDecimal("1.00")));
+        Property createdProperty = propertyRepository.createProperty(newProperty);
+
+        Optional<BigDecimal> expectedValue = Optional.of(new BigDecimal("14.000"));
+
+        String jsonExpected = objectMapper.writeValueAsString(expectedValue);
+
+        this.mockMvc
+            .perform(
+                get("/property/value/{id}", createdProperty.getId() - 1)
+                )
+            .andExpect(status().isOk())
             .andExpect(content().json(jsonExpected));
     }
 }
